e89559e39695c2e92cdab8f00768921a4bba9d13
dendariev <den.dariev@gmail.com>
2022-11-19 02:50:57 +0800

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Вы еще не совершеннолетний");
+        }
+    }
+}
