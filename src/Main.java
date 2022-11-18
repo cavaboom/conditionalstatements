420a113606133520b32128924523a45188b2f1cb
dendariev <den.dariev@gmail.com>
2022-11-19 03:04:32 +0800

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,17 @@ public class Main {
         if (age < 18) {
             System.out.println("Вы еще не совершеннолетний");
         }
+        // Задача 2
+        System.out.println("Задача 2");
+        int yearsOld = 25;
+        if (yearsOld >= 7 && yearsOld < 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (yearsOld >= 18 && yearsOld < 24) {
+            System.out.println("Человек может отправлятся в университет");
+        }
+        if (yearsOld >= 24) {
+            System.out.println("Человек окончил университет и может искать работу");
+        }
     }
 }
