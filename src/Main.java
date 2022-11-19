f3367fc98534f795c96b52df94e651b6483b4fb2
dendariev <den.dariev@gmail.com>
2022-11-20 01:33:58 +0800

    Все задачи

--- src/Main.java
+++ src/Main.java
@@ -49,13 +49,13 @@ public class Main {
         int seatPlacesUsed = 25;
         int standingPlacesUsed = 37;
         if (seatPlacesUsed < seatPlaces) {
-            System.out.println("Есть еще " +(seatPlaces - seatPlacesUsed)+ " сидячих мест");
+            System.out.println("Есть еще " + (seatPlaces - seatPlacesUsed) + " сидячих мест");
         }
         if (seatPlacesUsed == seatPlaces) {
             System.out.println("Сидячих мест нет");
         }
         if (standingPlacesUsed < standingPlaces) {
-            System.out.println("Есть еще " +(standingPlaces - standingPlacesUsed)+ " стоячих мест");
+            System.out.println("Есть еще " + (standingPlaces - standingPlacesUsed) + " стоячих мест");
         }
         if (standingPlacesUsed == standingPlaces) {
             System.out.println("Стоячих мест нет");
@@ -86,14 +86,73 @@ public class Main {
         int seatPlacesUsed_v2 = 25;
         int standingPlacesUsed_v2 = 36;
         if (seatPlacesUsed_v2 < seatPlaces_v2) {
-            System.out.println("Есть еще " +(seatPlaces - seatPlacesUsed)+ " сидячих мест");
+            System.out.println("Есть еще " + (seatPlaces - seatPlacesUsed) + " сидячих мест");
         } else {
             System.out.println("Сидячих мест нет");
         }
         if (standingPlacesUsed_v2 < standingPlaces_v2) {
-            System.out.println("Есть еще " +(standingPlaces - standingPlacesUsed)+ " стоячих мест");
+            System.out.println("Есть еще " + (standingPlaces - standingPlacesUsed) + " стоячих мест");
         } else {
             System.out.println("Стоячих мест нет");
+        }
+        // Задача 7
+        System.out.println("Задача 7");
+        int age_v3 = 25;
+        /*
+        boolean preSchool = age_v3 >= 2 && age_v3 <= 6;
+        boolean school = age_v3 > 6 && age_v3 <= 18;
+        boolean student = age_v3 > 18 && age_v3 <= 24;
+        boolean adult = age_v3 > 24;
+        if (preSchool) {
+            System.out.println("Если возраст человека равен " +age_v3+ " то ему нужно ходить в детский сад");
+        }
+        else if (school) {
+            System.out.println("Если возраст человека равен " +age_v3+ " то ему нужно ходить в школу");
+        }
+        else if (student) {
+            System.out.println("Если возраст человека равен " +age_v3+ " то ему нужно ходить в университет");
+        }
+        else if (adult) {
+            System.out.println("Если возраст человека равен " +age_v3+ " то ему нужно ходить на работу");
+        }
+
+         */
+        if (age_v3 >= 2 && age_v3 <= 6) {
+            System.out.println("Если возраст человека равен " + age_v3 + " то ему нужно ходить в детский сад");
+        } else if (age_v3 > 6 && age_v3 <= 18) {
+            System.out.println("Если возраст человека равен " + age_v3 + " то ему нужно ходить в школу");
+        } else if (age_v3 > 18 && age_v3 <= 24) {
+            System.out.println("Если возраст человека равен " + age_v3 + " то ему нужно ходить в университет");
+        } else if (age_v3 > 24) {
+            System.out.println("Если возраст человека равен " + age_v3 + " то ему нужно ходить на работу");
+        }
+        // Задача 8
+        System.out.println("Задача 8");
+        int ageOfTheChild = 15;
+        if (ageOfTheChild < 5) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        } else if (ageOfTheChild >= 5 && ageOfTheChild < 14) {
+            System.out.println("Ребенок может кататься на аттракционе только в сопровождении взрослого");
+        } else if (ageOfTheChild >= 14) {
+            System.out.println("Ребенок может кататься на аттракционе без сопровождения взрослого");
+        }
+        // Задача 9
+        System.out.println("Задача 9");
+        int one = 3;
+        int two = 1;
+        int three = 2;
+        if (one > two) {
+            if (one >= three) {
+                System.out.println("Большее число " + one);
+            } else {
+                System.out.println("Большее число " + three);
+            }
+        } else if (two > one) {
+            if (two > three) {
+                System.out.println("Большее число " + two);
+            } else {
+                System.out.println("Большее число " + three);
+            }
         }
     }
 }
